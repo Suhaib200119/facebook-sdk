99d550e3263e9957e4b7fb4bbcf0cedaf50cced7
Suhaib200119 <sis.22.18a@gmail.com>
2023-06-10 22:54:26 +0300

    first commit

---
+++ config/fcm.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'token' => "AAAAXS2NlH8:APA91bGm2G6SkRGBPcg2Yhk5EEtSWH8dscbFi6SMzOPxb8fZ21iPw-7Z4Z6h_S_vHMzcz29OeaCvDomr-KfMKRFBrHI0WeqdE11ojInzqvM2rzJGbk6089vY0dvjhI0wt8hajTLXLBSZ",
+];
+
+
+?>
